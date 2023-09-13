e6cfe9d46ab3563fa9611bdd2cdf1d32e0afca8a
lupinus03 <11mincheol@naver.com>
2023-09-13 17:34:25 +0900

    Update hello_wold.py

--- hello_wold.py
+++ hello_wold.py
@@ -1,7 +1,12 @@
 Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> def print_hello():
-... print("Hello World!!")
-... 
-... 
-... if __name__ == '__main__':
+def add (x,y):
+    a = x + y
+    return a
+
+def print_KIT():
+print("KIT")
+
+if __name__ == '__main__':
+    print_KIT()
+    print( print_hello(1,2) )
